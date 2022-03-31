3b0bb231bb361e352d0c52b673b4753b89608092
Riccardo Gamberini <gamberini@indacoproject.it>
2022-03-31 16:16:50 +0200

    Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServersComponent } from './servers/servers.component';
-import { FormsModule } from '@angular/forms';
+
+import { FormsModule } from '@angular/forms'; /* This line is necessary in order to use two-way-binding.*/
 import { MatTableModule } from '@angular/material/table';
 import { HeaderComponent } from './header/header.component';
 
+/*In order to user two-way-binding is also necessary to add FormsModule in imports section.*/
 @NgModule({
   declarations: [
     AppComponent,
