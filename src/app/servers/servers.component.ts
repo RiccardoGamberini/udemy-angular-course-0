ecc4977f6647493d64ed82c0b1b7113193813fde
Riccardo Gamberini <gamberini@indacoproject.it>
2022-03-31 14:53:13 +0200

    Commit.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,9 +5,30 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
-export class ServersComponent {
-  servers = [
-    {id: 0, name: 'server_0' },
-    {id: 1, name: 'server_1' }
-  ];
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  serversNamesExamples = ['test_server_0', 'test_server_1', 'test_server_2', 'test_server_3', 'test_server_4', 'test_server_5', 'test_server_6'];
+  seconds = 3;
+
+  ngOnInit() {
+    setInterval(() => {
+      let randomInteger = this.randomIntFromInterval(0, 5);
+      // In the following line "say" to the html file to change what is shown.
+      this.serverName = this.serversNamesExamples[randomInteger];
+    }, this.seconds * 1000);
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+  randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 }
