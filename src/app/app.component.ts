53387947417f378b87d8af2f8d0f5f2af5272147
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-04 15:19:57 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-angular-course-0';
+  aCondition: boolean;
+  conditions = [true, false, true, false, true, false, true, false];
+  imagePath = 'https://www.w3schools.com/images/picture.jpg';
+  buttonStatus: string;
+  seconds = 2;
+
+  ngOnInit() {
+    setInterval(() => {
+      let randomInteger = this.randomIntFromInterval(0, 5);
+      // The following line "says" to the html file to change what is shown.
+      this.aCondition = this.conditions[randomInteger];
+      this.buttonStatus = this.aCondition ? 'disable property button is true' : 'disabled property button is false';
+    }, this.seconds * 1000);
+  }
+
+  randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
 }
