821cee3f919982695dc1a20dbfbb28c591c64a52
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-05 16:11:33 +0200

    Commit.

--- src/app/custom-button/custom-button.component.ts
+++ src/app/custom-button/custom-button.component.ts
@@ -13,10 +13,11 @@ import {Component, EventEmitter, Output} from '@angular/core';
 export class CustomButtonComponent {
   /*
   Notes:
-  - The expression '@Output()' permits to components different from server-item component to receive events emitted by customEventEmitter.
+  - The expression '@Output('customEventAlias)' permits to components different from server-item component to receive events emitted by customEventEmitter.
+  - 'customEventAlias' is an alias for the name of the variable in which is stored the event emitter.
   - It's possible to instantiate an EventEmitter object not receivable by other components, in order to do so do not add @Output() decorator.
    */
-  @Output() customEvent = new EventEmitter<{ id: number, name: string }>();
+  @Output('customEventAlias') customEvent = new EventEmitter<{ id: number, name: string }>();
 
   eventEmissionFunction() {
     let max = 1000;
