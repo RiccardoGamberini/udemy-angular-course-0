6dab8d9fd0a29a412d9cc36dfe50ebd785b27c77
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-05 10:29:26 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'udemy-angular-course-0';
-}
+
+export class AppComponent implements  OnInit{
+  serverName: string;
+  serversNamesExamples = ['test_server_0', 'test_server_1', 'test_server_2', 'test_server_3', 'test_server_4', 'test_server_5', 'test_server_6'];
+  seconds = 2;
+
+  ngOnInit() {
+    setInterval(() => {
+      let randomInteger = this.randomIntFromInterval(0, 5);
+      // In the following line "say" to the html file to change what is shown.
+      this.serverName = this.serversNamesExamples[randomInteger];
+    }, this.seconds * 1000);
+  }
+
+  randomIntFromInterval(min, max) { // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }}
