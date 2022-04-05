b70dda2c87ada7ca47fd6c8ae2f3dcac4034874e
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-05 09:19:10 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-angular-course-0';
+  serverName: string;
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
 }
