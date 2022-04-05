5333b064f5e675c81ccd35bc6776ff106ee0abde
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-05 10:23:15 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,14 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  aCondition: boolean;
   imagePath = 'https://www.w3schools.com/images/picture.jpg';
-  seconds = 2;
-
-  ngOnInit() {
-    setInterval(() => {
-      // The following line "says" to the html file to change what is shown.
-      this.aCondition = Math.random() > 0.5 ? true : false;
-    }, this.seconds * 1000);
-  }
 }
