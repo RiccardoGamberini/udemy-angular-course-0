c4918b6fe8404ac6d9bc6dccb66aa67fbc649bc8
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-08 11:23:53 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  anArray = [
-    {id: 0, data: 'string_0'},
-    {id: 1, data: 'string_1'},
-    {id: 2, data: 'string_2'},
-    {id: 3, data: 'string_3'},
-    {id: 4, data: 'string_4'},
-    {id: 5, data: 'string_5'}
-  ];
-}
+export class AppComponent { }
