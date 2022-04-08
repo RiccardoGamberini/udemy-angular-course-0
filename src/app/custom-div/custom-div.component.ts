3cd6a539293d8758abba16a79f30d998757ba9f3
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-08 11:29:17 +0200

    Commit.

--- src/app/custom-div/custom-div.component.ts
+++ src/app/custom-div/custom-div.component.ts
@@ -13,16 +13,16 @@ export class CustomDivComponent {
   * of custom-div component (i'm talking about the opening and closing tags present in app.component.html). These HTML elements are called the content
   * of custom-div component. The content of custom-div component is a different thing than the view of custom-div component. Note that in the Angular documentation
   * there is the distinction of content and view (example: ViewChild and ContentChild).
-  * - The value of someParagraph variable (.....someParagraph: ElementRef.....) is the first HTML element that match the selector passed to @ContentChild.
+  * - The value of aParagraph variable (.....aParagraph: ElementRef.....) is the first HTML element that match the selector passed to @ContentChild.
   * - With ContentChild custom-div component can access to it's own content. It's own content is:
-  *   .....<p #someParagraph *ngFor="let item of anArray">Id = {{item.id}} | data = {{item.data}}</p>.....
+  *   .....<p #aParagraph *ngFor="let item of anArray">Id = {{item.id}} | data = {{item.data}}</p>.....
   * - It's own content is created by app component using values present in app.component.ts. So using ng-content and ContentChild i can transfer data
   *   a parent component (the app component) to a child component (the custom-div component).
   * */
-  @ContentChild('someParagraph') someParagraph: ElementRef;
+  @ContentChild('aParagraph') aParagraph: ElementRef;
 
   aFunction() {
-    console.log(this.someParagraph);
-    this.message = `The someParagraph ElementRef object is printed in console. It's value is: ${this.someParagraph.nativeElement.textContent}`;
+    console.log(this.aParagraph);
+    this.message = `The aParagraph ElementRef object is printed in console. It's value is: ${this.aParagraph.nativeElement.textContent}`;
   }
 }
