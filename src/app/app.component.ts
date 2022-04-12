84ac87059d59f221f0a537d7f6517300adddcd2d
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-12 10:37:51 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'udemy-angular-course-0';
+  loadedFeature = 'recipe';
+
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
