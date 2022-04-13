924ad474fce188d375b7162d9948c01a7a3e018d
RiccardoGamberini <riccardo.gamberini3@gmail.com>
2022-04-13 16:45:08 +0200

    Commit.

--- src/app/custom-button/custom-button.component.ts
+++ src/app/custom-button/custom-button.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: '[app-custom-button]',
+  selector: '.app-custom-button',
   templateUrl: './custom-button.component.html',
   styleUrls: ['./custom-button.component.css']
 })
