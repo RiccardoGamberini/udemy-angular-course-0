56b4e6060f075fdc474175e7bd5ffb02eb65f5e1
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-13 09:17:18 +0200

    Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
 import { MatTableModule } from '@angular/material/table';
 import { CustomButtonComponent } from './custom-button/custom-button.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
@@ -12,7 +13,8 @@ import { SidenavComponent } from './sidenav/sidenav.component';
   declarations: [
     AppComponent,
     CustomButtonComponent,
-    SidenavComponent
+    SidenavComponent,
+    MaterialModule
   ],
   imports: [
     BrowserModule,
