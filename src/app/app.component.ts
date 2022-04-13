154fd772d73889edbc26a2424705dbba91fddae5
Riccardo Gamberini <gamberini@indacoproject.it>
2022-04-13 12:53:46 +0200

    Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  aCondition = true;
+  customH2BaseClassName = 'customH2BaseClassName';
+  statusesNames = ['isActive', 'isInactive', 'isWaiting'];
+  currentH2StatusName = 'isInactive';
   ngOnInit() {
-    this.aCondition = Math.random() > 0.5 ? true : false;
+    let maximumRandomInteger = (this.statusesNames.length - 1);
+    let randomInteger = Math.floor(Math.random() * (maximumRandomInteger + 1));
+    this.currentH2StatusName = this.statusesNames[randomInteger];
   }
 }
