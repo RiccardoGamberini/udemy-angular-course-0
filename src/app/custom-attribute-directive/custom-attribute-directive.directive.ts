ee9476980956870165b6977d8543433d01d6a562
RiccardoGamberini <riccardo.gamberini3@gmail.com>
2022-04-14 12:09:36 +0200

    Commit.

--- src/app/custom-attribute-directive/custom-attribute-directive.directive.ts
+++ src/app/custom-attribute-directive/custom-attribute-directive.directive.ts
@@ -1,12 +1,16 @@
 import {Directive, ElementRef, OnInit} from '@angular/core';
 
-/* 
-The following selector name (.....[appCustomAttributeDirective].....) is a selector of type attribute. This type is called attribute because
+/*
+Only in the branches of this repository i will call attribute name the type of this selector. I think it's more clear than all the other names given
+by people to this type. */
+/*
+The following selector name (.....[appCustomAttributeDirective].....) is a selector of type attribute name. This type is called attribute name because
 in a HTML file is used the same way as all HTML built-in attributes that doesn't require a value. Example the disabled attribute.
 */
 @Directive({
   selector: '[appCustomAttributeDirective]'
 })
+/**/
 export class CustomAttributeDirectiveDirective implements OnInit {
   constructor(private elementRef: ElementRef) { }
   ngOnInit() {
