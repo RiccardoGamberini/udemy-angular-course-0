391f2b63808ddcf6f2261ea93f5dbddc53a2a5c2
RiccardoGamberini <riccardo.gamberini3@gmail.com>
2022-04-15 18:15:14 +0200

    Commit.

--- src/app/custom-structural-directive/custom-structural-directive.directive.ts
+++ src/app/custom-structural-directive/custom-structural-directive.directive.ts
@@ -1,14 +1,14 @@
 import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
-  selector: '[appUnless'
+  selector: '[appUnless]'
 })
 export class CustomStructuralDirectiveDirective {
   @Input() set appUnless(aConditionName: boolean) {
     /*Depending on the value of aConditionName
-    - the HTML element (templateRef) on which the directive is placed will be inserted in a view container (viewContainerRef) in order to be added to the DOM.
-    or
-    - every TemplateRef object eventually previously inserted in viewContainerRef will be remove from viewContainerRef.
+    * - the HTML element (templateRef) on which the directive is placed will be inserted in a view container (viewContainerRef) in order to be added to the DOM.
+    * or
+    * - every TemplateRef object eventually previously inserted in viewContainerRef will be remove from viewContainerRef.
     * */
     if(!aConditionName) {
       this.viewContainerRef.createEmbeddedView(this.templateRef);
