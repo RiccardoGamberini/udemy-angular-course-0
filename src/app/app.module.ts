b07353efe831d7cf82217d11deeb207b874b4fb3
RiccardoGamberini <riccardo.gamberini3@gmail.com>
2022-04-20 15:48:53 +0200

    Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,7 @@ import { MaterialModule } from 'material.module';
 
 import { AppComponent } from './app.component';
 import { CustomDivComponent } from './custom-div/custom-div.component';
-import { AService } from "./aService.service";
 
-/*In order to have inject the aService at app.module.ts level i have to:
-* - Insert the name of the service class in providers array (.....providers: [AService].....).
-* - Add a line in which i import aService.service.ts (.....import {AService} from "./aService.service";
-.....)*/
 @NgModule({
   declarations: [
     AppComponent,
