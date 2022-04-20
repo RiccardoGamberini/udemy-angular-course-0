7c6f47282d2ee9c74d5ba96263f619e0fbfb2719
RiccardoGamberini <riccardo.gamberini3@gmail.com>
2022-04-20 13:15:35 +0200

    Commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from 'material.module';
+
+import { AppComponent } from './app.component';
 import { CustomDivComponent } from './custom-div/custom-div.component';
 import { AService } from "./aService.service";
 
