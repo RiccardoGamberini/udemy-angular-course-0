f24448c32a557163f7483a90cc9064e037d29c26
RiccardoGamberini <riccardo.gamberini3@gmail.com>
2022-04-20 14:05:58 +0200

    Commit.

--- src/app/aService.service.ts
+++ src/app/aService.service.ts
@@ -1,3 +1,15 @@
+/*In this branch the injection of aService service in anotherService service without the following lines:
+* .....
+* import { Injectable } from "@angular/core";
+* .....
+* @Injectable()
+* .....
+* However it's recommended to add these two lines because in future Angular might require them in order to inject a service into another service correctly.
+* */
+import { Injectable } from "@angular/core";
+
+@Injectable()
+
 export class AService {
   aFunction(aString: string) {
     console.log(aString);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AnotherService} from "./anotherService.service";
+import { AnotherService } from "./anotherService.service";
 
 @Component({
   selector: 'app-root',
